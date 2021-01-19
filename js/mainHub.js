46c8c1d86ac616f3becade9c83e7ef54a74104fe
Ihor-Ihorevych <77419426+Ihor-Ihorevych@users.noreply.github.com>
2021-01-19 06:24:00 +0300

    fixes
    minor fixes

--- js/mainHub.js
+++ js/mainHub.js
@@ -1,15 +1,16 @@
 import showPromoText from './timer_show_hidden_text.js'
-import mobileNav from  './header_bullshirt.js'
+import mobileNav from './header_bullshirt.js'
 import placeStars from './Arrow_Star.js'
 import StartTimer from './Timer.js'
-$(document).ready(function () {
+$(document).ready(function() {
     let demoPromos = [
         `Мы оформим вам бесплатную <br>доставку в любую точку Украины...`,
         `Мы оформим Вам бесплатную доставку в любую точку Украины,
         при  условии приобретения 3 любых рубашек на выбор,
         от нашей компании BullShirt! Акция действует 
         только при оформлении покупки через наш сайт!
-    `]
+    `
+    ]
     showPromoText(demoPromos);
 
     mobileNav($('.menu-button'), $('.burger_menu'))
